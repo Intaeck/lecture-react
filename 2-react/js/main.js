4ebd16f7ceb5de4d0405c0ab1b0863ac896fadab
intaeck <intaeck@gmail.com>
2021-11-23 21:44:26 +0900

    211123

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -4,7 +4,8 @@ class App extends React.Component {
 
     this.state = {
       searchKeyword: "",
-      // TODO
+      // 검색결과
+      searchResult: [],
     };
   }
 
@@ -27,25 +28,33 @@ class App extends React.Component {
     return (
       <>
         <header>
-          <h2 className="container">검색</h2>
+          <h2 className='container'>검색</h2>
         </header>
-        <div className="container">
+        <div className='container'>
           <form
             onSubmit={(event) => this.handleSubmit(event)}
             onReset={() => this.handleReset()}
           >
             <input
-              type="text"
-              placeholder="검색어를 입력하세요"
+              type='text'
+              placeholder='검색어를 입력하세요'
               autoFocus
               value={this.state.searchKeyword}
               onChange={(event) => this.handleChangeInput(event)}
             />
             {this.state.searchKeyword.length > 0 && (
-              <button type="reset" className="btn-reset"></button>
+              <button type='reset' className='btn-reset'></button>
             )}
           </form>
-          <div className="content">{/* TODO */}</div>
+
+          {/* 검색결과 */}
+          <div className='content'>
+            {this.state.searchResult.length > 0 ? (
+              <div>TODO: 검색 결과 목록 표시하기</div>
+            ) : (
+              <div className='empty-box'>검색 결과가 없습니다</div>
+            )}
+          </div>
         </div>
       </>
     );
