e0d97147ea1c299ff28002626405e7d8a7cf5047
intaeck <intaeck@gmail.com>
2021-11-26 21:32:29 +0900

    211126

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,3 +1,4 @@
+import { formatRelativeDate } from './js/helpers.js';
 import store from "./js/Store.js";
 
 const TabType = {
@@ -20,12 +21,14 @@ class App extends React.Component {
       submitted: false,
       selectedTab: TabType.KEYWORD,
       keywordList: [],
+      historyList: [],
     };
   }
 
   componentDidMount() {
     const keywordList = store.getKeywordList();
-    this.setState({ keywordList });
+    const historyList = store.getHistoryList();
+    this.setState({ keywordList, historyList });
   }
 
   handleSubmit(event) {
@@ -36,7 +39,8 @@ class App extends React.Component {
   search(searchKeyword) {
     const searchResult = store.search(searchKeyword);
     this.setState({
-      // TODO
+      // searchKeyword를 update하면 바인딩 된 searchform에도 반영해준다
+      searchKeyword,
       searchResult,
       submitted: true,
     });
@@ -70,6 +74,7 @@ class App extends React.Component {
           type="text"
           placeholder="검색어를 입력하세요"
           autoFocus
+          // searchKeyword 와 binding 되어있다
           value={this.state.searchKeyword}
           onChange={(event) => this.handleChangeInput(event)}
         />
@@ -96,8 +101,8 @@ class App extends React.Component {
     const keywordList = (
       <ul className="list">
         {this.state.keywordList.map(({ id, keyword }, index) => (
-          // TODO
-          <li key={id}>
+          // event 처리
+          <li key={id} onClick={() => this.search(keyword)}>
             <span className="number">{index + 1}</span>
             <span>{keyword}</span>
           </li>
@@ -105,23 +110,36 @@ class App extends React.Component {
       </ul>
     );
 
+    const historyList = (
+      <ul className="list">
+        {this.state.historyList.map(({ id, keyword, date }) => {
+          return (
+            <li key={id} onClick={() => this.search(keyword)}>
+              <span>{keyword}</span>
+              <span className="date">{formatRelativeDate(date)}</span>
+              <button className="btn-remove"></button>
+            </li>
+          )
+        })}
+      </ul>
+    );
+
     const tabs = (
       <>
-        <ul className="tabs">
+        <ul className='tabs'>
           {Object.values(TabType).map((tabType) => (
             <li
               key={tabType}
-              className={this.state.selectedTab === tabType ? "active" : ""}
+              className={this.state.selectedTab === tabType ? 'active' : ''}
               onClick={() => this.setState({ selectedTab: tabType })}
             >
               {TabLabel[tabType]}
             </li>
           ))}
         </ul>
+        {/* && 논리연산자 사용 */}
         {this.state.selectedTab === TabType.KEYWORD && keywordList}
-        {this.state.selectedTab === TabType.HISTORY && (
-          <>{`TODO: 최근 검색어`}</>
-        )}
+        {this.state.selectedTab === TabType.HISTORY && historyList}
       </>
     );
 
