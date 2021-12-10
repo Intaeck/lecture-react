9145995357198189004ff67f608b945fb4b424da
intaeck <intaeck@gmail.com>
2021-12-10 13:37:12 +0900

    211209

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -1,25 +1,62 @@
-import React from "react";
-import Header from "./components/Header.js";
-import SearchForm from "./components/SearchForm.js";
+import React from 'react';
+import Header from './components/Header.js';
+import SearchForm from './components/SearchForm.js';
+import SearchResult from './components/SearchResult.js';
+import store from './Store.js';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    // state 끌어올리기 -> SearchForm에 있던 searchKeyword를 App에서 관리
+    this.state = { searchKeyword: '', searchResult: [], submitted: false };
+  }
+
   search(searchKeyword) {
-    console.log("TODO: search", searchKeyword);
+    console.log('TODO: search', searchKeyword);
+    const searchResult = store.search(searchKeyword);
+
+    this.setState({
+      searchResult,
+      submitted: true,
+    });
   }
 
   handleReset() {
-    console.log("TODO: reset");
+    console.log('TODO: reset');
+    // 초기화 처리
+    this.setState({
+      searchKeyword: '',
+      submitted: false,
+      searchResult: [],
+    })
+  }
+
+  handleChangeInput(searchKeyword) {
+    if (searchKeyword.length <= 0) {
+      this.handleReset();
+    }
+    this.setState({ searchKeyword });
   }
 
   render() {
+    const { searchKeyword, submitted, searchResult } = this.state;
+
     return (
       <>
-        <Header title="검색" />
-        <div className="container">
+        <Header title='검색' />
+        <div className='container'>
           <SearchForm
-            onSubmit={(searchKeyword) => this.search(searchKeyword)}
+            // 하단의 value 및 콜백함수들은 SearchForm에서 props로 접근이 가능하다
+            value={this.state.searchKeyword}
+            // 변경된 value가 넘어오면 처리하는 함수 -> state 변경처리
+            onChange={(value) => this.handleChangeInput(value)}
+            onSubmit={() => this.search(searchKeyword)}
             onReset={() => this.handleReset()}
           />
+          {/* 검색결과 */}
+          <div className="content">
+            {submitted && <SearchResult data={searchResult} />}
+          </div>
         </div>
       </>
     );
--- 3-component/src/components/SearchForm.js
+++ 3-component/src/components/SearchForm.js
@@ -1,50 +1,85 @@
 import React from "react";
 
-export default class SearchForm extends React.Component {
-  constructor() {
-    super();
-
-    this.state = { searchKeyword: "" };
-  }
-
-  handleSubmit(event) {
+// state관리를 부모인 App.js로 넘겼으므로 state 관리가 필요없는 함수형 컴포넌트로 변경
+// props를 App.js로 부터 받음
+const SearchForm = (props) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    this.props.onSubmit(this.state.searchKeyword);
-  }
+    // props로 전달된 onSubmit 함수 호출
+    props.onSubmit();
+  };
 
-  handleReset() {
-    this.props.onReset();
-  }
+  const handleReset = () => {
+    props.onReset();
+  };
 
-  handleChangeInput(event) {
-    const searchKeyword = event.target.value;
+  const handleChangeInput = (event) => {
+    props.onChange(event.target.value);
+  };
 
-    if (searchKeyword.length <= 0) {
-      return this.handleReset();
-    }
+  // 함수형 컴포넌트는 리엑트 엘리먼트를 반환해야함
+  return (
+    <form onSubmit={handleSubmit} onReset={handleReset}>
+      <input
+        type='text'
+        placeholder='검색어를 입력하세요'
+        autoFocus
+        value={props.value}
+        onChange={handleChangeInput}
+      />
+      {props.value.length > 0 && <button type='reset' className='btn-reset' />}
+    </form>
+  );
+};
 
-    this.setState({ searchKeyword });
-  }
+export default SearchForm;
 
-  render() {
-    const { searchKeyword } = this.state;
+// export default class SearchForm extends React.Component {
+//   constructor() {
+//     super();
 
-    return (
-      <form
-        onSubmit={(event) => this.handleSubmit(event)}
-        onReset={() => this.handleReset()}
-      >
-        <input
-          type="text"
-          placeholder="검색어를 입력하세요"
-          autoFocus
-          value={searchKeyword}
-          onChange={(event) => this.handleChangeInput(event)}
-        />
-        {searchKeyword.length > 0 && (
-          <button type="reset" className="btn-reset" />
-        )}
-      </form>
-    );
-  }
-}
+//     // 부모인 App.js로 state 위임
+//     // this.state = { searchKeyword: "" };
+//   }
+
+//   handleSubmit(event) {
+//     event.preventDefault();
+//     this.props.onSubmit(this.state.searchKeyword);
+//   }
+
+//   handleReset() {
+//     this.props.onReset();
+//   }
+
+//   handleChangeInput(event) {
+//     const searchKeyword = event.target.value;
+
+//     if (searchKeyword.length <= 0) {
+//       return this.handleReset();
+//     }
+
+//     this.setState({ searchKeyword });
+//   }
+
+//   render() {
+//     const { searchKeyword } = this.state;
+
+//     return (
+//       <form
+//         onSubmit={(event) => this.handleSubmit(event)}
+//         onReset={() => this.handleReset()}
+//       >
+//         <input
+//           type="text"
+//           placeholder="검색어를 입력하세요"
+//           autoFocus
+//           value={searchKeyword}
+//           onChange={(event) => this.handleChangeInput(event)}
+//         />
+//         {searchKeyword.length > 0 && (
+//           <button type="reset" className="btn-reset" />
+//         )}
+//       </form>
+//     );
+//   }
+// }
